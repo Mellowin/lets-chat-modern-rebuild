08309714ea4150cc4378dd0003203313df96b26e
Mellow <mellowin1987@gmail.com>
2026-06-24 16:35:57 +0300

    fix(api/contacts): import AuthModule to resolve JwtAccessGuard dependencies in E2E

--- apps/api/src/contacts/contacts.module.ts
+++ apps/api/src/contacts/contacts.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { DirectConversationsModule } from '../direct-conversations/direct-conversations.module';
 import { ContactsController } from './contacts.controller';
@@ -6,7 +7,7 @@ import { ContactsService } from './contacts.service';
 import { ContactsRepository } from './contacts.repository';
 
 @Module({
-  imports: [UsersModule, DirectConversationsModule],
+  imports: [AuthModule, UsersModule, DirectConversationsModule],
   controllers: [ContactsController],
   providers: [ContactsService, ContactsRepository],
   exports: [ContactsService, ContactsRepository],
